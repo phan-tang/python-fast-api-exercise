b32adb1fd3e5da869474df57bef59670378ef339
Tang Hien Phan <phan05042000@gmail.com>
2023-04-19 18:14:37 +0700

    Add code for database connection, add base entity

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,24 +1,30 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
+from setting import SQLALCHEMY_URL
+from database import metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+fileConfig(config.config_file_name)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
+
+# DB connection string
+config.set_main_option('sqlalchemy.url', SQLALCHEMY_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ app/setting.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def generate_db_connection_string():
+    engine = os.environ.get('DB_ENGINE')
+    username = os.environ.get('DB_USERNAME')
+    password = os.environ.get('DB_PASSWORD')
+    dbhost = os.environ.get('DB_HOST')
+    dbname = os.environ.get('DB_NAME')
+    return f"{engine}://{username}:{password}@{dbhost}/{dbname}"
+
+
+SQLALCHEMY_URL = generate_db_connection_string()
