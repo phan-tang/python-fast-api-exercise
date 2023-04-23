24e4636c44bc455c5ea381aa2c5a78856eca5a12
Tang Hien Phan <phan05042000@gmail.com>
2023-04-23 20:45:47 +0700

    Add seeder for user table

--- app/alembic/versions/a2df7e9765d2_generate_user_table.py
+++ app/alembic/versions/a2df7e9765d2_generate_user_table.py
@@ -8,6 +8,11 @@ Create Date: 2023-04-19 11:19:29.311605
 from alembic import op
 import sqlalchemy as sa
 
+from datetime import datetime
+from uuid import uuid4
+
+from schemas import get_password_hash
+from settings import ADMIN_DEFAULT_PASSWORD
 
 # revision identifiers, used by Alembic.
 revision = 'a2df7e9765d2'
@@ -17,22 +22,38 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    table = op.create_table(
         'users',
         sa.Column('id', sa.UUID, nullable=False, primary_key=True),
-        sa.Column('company_id', sa.UUID, nullable=False),
+        sa.Column('company_id', sa.UUID),
         sa.Column('email', sa.String(255), unique=True, nullable=False),
         sa.Column('username', sa.String(255), unique=True, nullable=False),
         sa.Column('first_name', sa.String(255), nullable=False),
         sa.Column('last_name', sa.String(255), nullable=False),
-        sa.Column('hashed_password', sa.String, nullable=False),
+        sa.Column('password', sa.String, nullable=False),
         sa.Column('is_active', sa.Boolean, default=1),
         sa.Column('is_admin', sa.Boolean, default=0),
+        sa.Column('is_superadmin', sa.Boolean, default=0),
         sa.Column('created_at', sa.DateTime),
         sa.Column('updated_at', sa.DateTime)
     )
     op.create_foreign_key('fk_user_company', 'users',
                           'companies', ['company_id'], ['id'])
+
+    op.bulk_insert(table, [
+        {
+            "id": uuid4(),
+            "email": "superadmin@email.com",
+            "username": "superadmin",
+            "first_name": "Superadmin",
+            "last_name": "System",
+            "password": get_password_hash(ADMIN_DEFAULT_PASSWORD),
+            "is_active": True,
+            "is_admin": False,
+            "is_superadmin": True,
+            "created_at": datetime.utcnow(),
+        }
+    ])
 
 
 def downgrade() -> None:
